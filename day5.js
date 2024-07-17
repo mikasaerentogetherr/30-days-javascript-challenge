4f2de1cfa77213f1b68d7443c0ef454bfad2d207
MIKASA <mikasaerentogether@proton.me>
2024-07-17 13:32:14 +0530

    Update day5.js

--- day5.js
+++ day5.js
@@ -6,116 +6,116 @@
 
 function EvenAndOdd() {
    if(num % 2 == 0){
-        console.log("Even")
-           }
-             else{
-                 console.log("Odd")
-                   }
-                   }
+     console.log("Even")
+   }
+  else{
+    console.log("Odd")
+  }
+}
 
-                   let num = 15
-                   EvenAndOdd(num)
+let num = 15
+EvenAndOdd(num)
 
-                   // Task 2
+// Task 2
 
-                   function square(num1){
-                     return num1 * num1
-                     }
+function square(num1){
+  return num1 * num1
+}
 
-                     console.log(square(5))
+console.log(square(5))
 
-                     // Activity 2: Function Expression
+// Activity 2: Function Expression
 
-                     // Task 3
+// Task 3
 
-                     function maxNum(num1, num2){
-                       if(num1 > num2){
-                           console.log(`The maximum number is ${num1}`)
-                             }
-                               else if (num2 > num1){
-                                   console.log(`The maximum number is ${num2}`)
-                                     }
-                                       else{
-                                           consle.log("Both numbers are equal")
-                                             }
-                                             }
+function maxNum(num1, num2){
+  if(num1 > num2){
+    console.log(`The maximum number is ${num1}`)
+  }
+  else if (num2 > num1){
+    console.log(`The maximum number is ${num2}`)
+  }
+  else{
+    consle.log("Both numbers are equal")
+  }
+}
 
-                                             maxNum(10, 20)
+maxNum(10, 20)
 
-                                             // Task 4
+// Task 4
 
-                                             function concatString(str1, str2){
-                                               return str1 + str2
-                                               }
+function concatString(str1, str2){
+  return str1 + str2
+}
 
-                                               let string = concatString("Hello", "World")
+let string = concatString("Hello", "World")
 
-                                               console.log(string)
+console.log(string)
 
-                                               // Activity 3: Arrow Function
+// Activity 3: Arrow Function
 
-                                               // Task 5
+// Task 5
 
-                                               function add(num1, num2){
-                                                 return num1 + num2
-                                                 }
+function add(num1, num2){
+  return num1 + num2
+}
 
-                                                 let sum = add(10, 20)
+let sum = add(10, 20)
 
-                                                 console.log(sum)
+console.log(sum)
 
-                                                 // Task 6
+// Task 6
 
-                                                 let strCheck = (str,char) => {
-                                                   return str.includes(char)
-                                                   }
+let strCheck = (str,char) => {
+  return str.includes(char)
+}
 
-                                                   console.log(strCheck("Hello", "o"))
+console.log(strCheck("Hello", "o"))
 
-                                                   // Activity 4 : Function Parameters & Default Vaules
+// Activity 4 : Function Parameters & Default Vaules
 
-                                                   // Task 7
+// Task 7
 
-                                                   let product = (num1, num2=9) => {
-                                                     return num1 * num2
-                                                     }
+let product = (num1, num2=9) => {
+  return num1 * num2
+}
 
-                                                     let result = product(10)
-                                                     console.log(result)
+let result = product(10)
+console.log(result)
 
-                                                     // Task 8
+// Task 8
 
-                                                     let greet =(name, age=21)=>(`Hello ${name}, you are ${age} years old`)
+let greet =(name, age=21)=>(`Hello ${name}, you are ${age} years old`)
 
-                                                     console.log(greet("John"))
+console.log(greet("John"))
 
-                                                     // Activity 5: Higher order funcitons
+// Activity 5: Higher order funcitons
 
-                                                     //Task 9
+//Task 9
 
-                                                     function higher(fun, number) {
-                                                         return fun(number) * fun(number)
-                                                         };
-                                                         const fun = function (number) {
-                                                             return number * number;
-                                                             };
-                                                             const result2 = higher(fun, 8);
-                                                             console.log(result2);
+function higher(fun, number) {
+    return fun(number) * fun(number)
+};
+const fun = function (number) {
+    return number * number;
+};
+const result2 = higher(fun, 8);
+console.log(result2);
 
-                                                             //Task 10
+//Task 10
 
-                                                             function higherOrder(fun2, fun3, value) {
-                                                                 const result = fun2(value);
-                                                                     const anotherResult = fun3(result);
-                                                                         console.log(anotherResult);
-                                                                         }
+function higherOrder(fun2, fun3, value) {
+    const result = fun2(value);
+    const anotherResult = fun3(result);
+    console.log(anotherResult);
+}
 
-                                                                         function fun2(value) {
-                                                                             return value - 2;
-                                                                             }
-                                                                             function fun3(resu) {
-                                                                                 return resu - 3;
-                                                                                 }
-                                                                                 const value = 20;
+function fun2(value) {
+    return value - 2;
+}
+function fun3(resu) {
+    return resu - 3;
+}
+const value = 20;
 
-                                                                                 higherOrder(fun2, fun3, value)
+higherOrder(fun2, fun3, value)
